fccd25a2a96958ac8bfffc64863a1814e599e338
Mazlen Zulleikhaa <100333611+Nekoir@users.noreply.github.com>
2022-12-21 16:00:32 +0800

    Update base-coding.c

--- base-coding.c
+++ base-coding.c
@@ -1,5 +1,13 @@
 #include "stm32f4xx.h"
 
+// Morse code definitions
+#define DOT 500
+#define DASH 1000
+#define INTER_LETTER 500
+#define INTER_WORD 1000
+
+const char* message = "HELLO WORLD";
+
 // Define the Morse code alphabet
 const char* morse_code[] = {
   ".-",   // A
@@ -30,29 +38,32 @@ const char* morse_code[] = {
   "--.."  // Z
 };
 
-// Define the output pin and initialize it to low
-#define LED_PIN GPIO_Pin_12
-#define LED_PORT GPIOD
-#define LED_ON()  GPIO_SetBits(LED_PORT, LED_PIN)
-#define LED_OFF() GPIO_ResetBits(LED_PORT, LED_PIN)
-
 int main(void)
 {
-  // Initialize the output pin
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-  GPIO_InitTypeDef gpio_init;
-  gpio_init.GPIO_Pin = LED_PIN;
-  gpio_init.GPIO_Mode = GPIO_Mode_OUT;
-  gpio_init.GPIO_OType = GPIO_OType_PP;
-  gpio_init.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(LED_PORT, &gpio_init);
-  LED_OFF();
-
-  // Encode a message in Morse code
-  const char* message = "HELLO WORLD";
-  while (*message) {
-    char c = *message++;
-
-    // Make sure the character is uppercase
+  // Initialize GPIO
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // Enable GPIOD clock
+  GPIOD->MODER |= (1 << 24); // Set GPIOD pin 12 to output mode
+  
+  while (1) {
+    // Blink LED in Morse code
+    for (int i = 0; i < strlen(message); i++) {
+      char c = message[i];
+      if (c == ' ') {
+        delay(INTER_LETTER * 3); // Delay between words
+      } else {
+        for (int j = 0; j < strlen(MORSE_CODE[c - 'A']); j++) {
+          GPIOD->ODR |= (1 << 12); // Turn on LED
+          char code = MORSE_CODE[c - 'A'][j];
+          if (code == '.') {
+            delay(DOT);
+          } else if (code == '-') {
+            delay(DASH);
+          }
+          GPIOD->ODR &= ~(1 << 12); // Turn off LED
+          delay(DOT); // Delay between dots and dashes
+        }
+        delay(INTER_LETTER); // Delay between letters
+      }
+    }
   }
+}
